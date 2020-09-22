802c1ff7a97e5ee6f1043cce852d5c0ae0706b0b
Patricname <52151233+Patricname@users.noreply.github.com>
2020-09-22 23:11:41 +0300

    Update Name.java
    Добавил цикл

--- Name.java
+++ Name.java
@@ -1,5 +1,9 @@
 public class Name {
     public static void main(String[] args) {
+        int x = 3;
+        int y = x;
+        for (int i = 0; i < x; i++){
         System.out.println (" Hello world");
+        }
     }
 }
